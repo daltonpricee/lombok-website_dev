e5c16fd5b0a2b6e43e522127ff0e0a0343eb46dd
Dalton Price <daltonpricee@gmail.com>
2025-09-02 20:48:16 -0700

    added in more pages with content

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ console.log("Lombok Website Loaded");
 
 document.addEventListener("DOMContentLoaded", () => {
   const title = document.getElementById("title");
-  title.style.transition = "transform 1s ease";
-  title.style.transform = "scale(1.1)";
+  if(title){
+    title.style.transition = "transform 1s ease";
+    title.style.transform = "scale(1.1)";
+  }
 });
 
 // Enrollment form submission
@@ -13,7 +15,6 @@ if (enrollForm) {
   enrollForm.addEventListener("submit", function(e) {
     e.preventDefault(); // Prevent page refresh
     alert("We got you in!");
-    // Later: Here you can send form data to a database
     enrollForm.reset(); // Clear the form
   });
 }
